b1535df87c38ce498fad9b5aa348ef31b87a18b4
DINNERman <shrwnwilliams@gmail.com>
2021-05-18 19:12:15 -0400

    finished the work and workcard components

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -4,9 +4,10 @@ function WorkCard({ title, img, url, alt }) {
   return (
     <div>
       <p>
-        <a href={url} target="blank" className="text-info">
-            {title}<br/>
-      <img src={img} alt={alt} className="img-fluid work-img" />
+        <a href={url} target="blank" className="text-info nav-link">
+          <h3>{title}</h3>
+          <br />
+          <img src={img} alt={alt} className="img-fluid work-img" />
         </a>
       </p>
     </div>
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -3,32 +3,63 @@ import WorkCard from "../components/WorkCard";
 import WorkInfo from "../work.json";
 
 function Work() {
-    
-
   return (
-    <div className="container text-center mt-5">
+    <div className="container text-center mt-3">
+      <div className="border-bottom border-info mb-3">
+        <h2 className="mb-3">About Me</h2>
+      </div>
       <div className="row mb-2">
         <div className="col">
-          <WorkCard title={WorkInfo[0].title} img={WorkInfo[0].img} url={WorkInfo[0].url} alt={WorkInfo[0].alt} />
-        </div> 
-          <div className="col"> 
-          <WorkCard title={WorkInfo[1].title} img={WorkInfo[1].img} url={WorkInfo[1].url} alt={WorkInfo[1].alt} />
-        </div> 
-      </div> 
-      <div className="row mb-2"> 
-        <div className="col"> 
-          <WorkCard title={WorkInfo[2].title} img={WorkInfo[2].img} url={WorkInfo[2].url} alt={WorkInfo[2].alt} />
-        </div> 
-          <div className="col"> 
-          <WorkCard title={WorkInfo[3].title} img={WorkInfo[3].img} url={WorkInfo[3].url} alt={WorkInfo[3].alt} />
-        </div> 
-      </div> 
-      <div className="row mb-2"> 
-        <div className="col"> 
-          <WorkCard title={WorkInfo[4].title} img={WorkInfo[4].img} url={WorkInfo[4].url} alt={WorkInfo[4].alt} />
-        </div> 
-          <div className="col"> 
-          <WorkCard title={WorkInfo[5].title} img={WorkInfo[5].img} url={WorkInfo[5].url} alt={WorkInfo[5].alt} />
+          <WorkCard
+            title={WorkInfo[0].title}
+            img={WorkInfo[0].img}
+            url={WorkInfo[0].url}
+            alt={WorkInfo[0].alt}
+          />
+        </div>
+        <div className="col">
+          <WorkCard
+            title={WorkInfo[1].title}
+            img={WorkInfo[1].img}
+            url={WorkInfo[1].url}
+            alt={WorkInfo[1].alt}
+          />
+        </div>
+      </div>
+      <div className="row mb-2">
+        <div className="col">
+          <WorkCard
+            title={WorkInfo[2].title}
+            img={WorkInfo[2].img}
+            url={WorkInfo[2].url}
+            alt={WorkInfo[2].alt}
+          />
+        </div>
+        <div className="col">
+          <WorkCard
+            title={WorkInfo[3].title}
+            img={WorkInfo[3].img}
+            url={WorkInfo[3].url}
+            alt={WorkInfo[3].alt}
+          />
+        </div>
+      </div>
+      <div className="row mb-2">
+        <div className="col">
+          <WorkCard
+            title={WorkInfo[4].title}
+            img={WorkInfo[4].img}
+            url={WorkInfo[4].url}
+            alt={WorkInfo[4].alt}
+          />
+        </div>
+        <div className="col">
+          <WorkCard
+            title={WorkInfo[5].title}
+            img={WorkInfo[5].img}
+            url={WorkInfo[5].url}
+            alt={WorkInfo[5].alt}
+          />
         </div>
       </div>
     </div>
