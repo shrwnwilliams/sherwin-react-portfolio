8a3fcfd981f33062ff662a831a70908d6a963ef5
DINNERman <shrwnwilliams@gmail.com>
2021-11-02 18:43:20 -0400

    added xyelthan and changed info to story on Jalana to hopefully get the info to show up on the page

--- src/components/DnDCard.js
+++ src/components/DnDCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function DndCard({ id, name, img, img2, info }) {
+function DndCard(params) {
   return (
     <div className="card">
-      <img className="card-img-top img-thumbnail" src={img} alt={name} />
-      <div className="card-header h3">{name}</div>
+      <img className="card-img-top img-thumbnail" src={params.img} alt={params.name} />
+      <div className="card-header h3">{params.name}</div>
       <div className="card-body">
-      <p className="card-text">{info}butt stuff</p>
+      <p className="card-text">{params.info}butt stuff</p>
       </div>
     </div>
   );
